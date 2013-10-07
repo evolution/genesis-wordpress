aad40f7ce70a3f232a344891e6204e8a00e896d4
Eric Clemmons <eric@smarterspam.com>
2013-10-07 18:26:55 -0500

    ssh now executes using role web

--- deployment/lib/ssh.rb
+++ deployment/lib/ssh.rb
@@ -1,8 +1,10 @@
 namespace :genesis do
     namespace :ssh do
         desc "SSH into machine"
-        task :default do
-            system "ssh #{user}@#{stage}.#{domain} -i ./provisioning/files/ssh/id_rsa"
+        task :default, :roles => :web do
+            find_servers_for_task(current_task).each do |current_server|
+                system "ssh #{user}@#{current_server} -i ./provisioning/files/ssh/id_rsa"
+            end
         end
     end
 end
