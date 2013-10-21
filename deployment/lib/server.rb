9e1095ef4cf198fb154122e1a7f8899b749f141c
Eric Clemmons <eric@smarterspam.com>
2013-10-21 10:59:36 -0500

    Whoops.  Run genesis:permissions on server, not local

--- deployment/lib/server.rb
+++ deployment/lib/server.rb
@@ -22,8 +22,8 @@ namespace :genesis do
 
     desc "Fix permissions"
     task :permissions do
-        exec "find #{remote_web} -follow -type d -exec chmod 755 {} \\;"
-        exec "find #{remote_web} -follow -type f -exec chmod 644 {} \\;"
+        run "find #{remote_web} -follow -type d -exec chmod 755 {} \\;"
+        run "find #{remote_web} -follow -type f -exec chmod 644 {} \\;"
     end
 
     namespace :logs do
