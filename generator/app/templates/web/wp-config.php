2266dbd3a224cc3855fc5eec4b58b51c18c38695
Evan Kaufman <ekaufman@collegedegrees.com>
2013-11-08 14:34:42 -0600

    Added FS_METHOD setting of "direct"
    This makes sense since the server we're provisioning has neither ftp nor keyless ssh support, and permissions should allow for direct filesystem io

--- generator/app/templates/web/wp-config.php
+++ generator/app/templates/web/wp-config.php
@@ -18,8 +18,8 @@ require_once(dirname(__FILE__) . '/../bower_components/genesis-wordpress/src/Gen
   // Replace salts
   .replace(/(\/\*\*#@\+.+?\*\/\n).+?(\n\/\*\*#@-\*\/)/m, "$1" + props.salts + "$2")
 
-  // Limit to 5 post revisions
-  .replace("/* That's all,", "define('WP_POST_REVISIONS', 5);\n\n/*That's all,")
+  // Limit to 5 post revisions, and force direct filesystem IO
+  .replace("/* That's all,", "define('WP_POST_REVISIONS', 5);\n\ndefine('FS_METHOD', 'direct');\n\n/*That's all,")
 %>
 if (WP_ENV !== 'www') {
   Genesis::rewriteUrls();
