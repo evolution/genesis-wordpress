5177bb60fcc44e769d9ed19a991616cf8519c88e
Eric Clemmons <eric@smarterspam.com>
2013-12-18 17:41:50 -0800

    Install latest genesis-wordpress branch

--- test/support/generator.js
+++ test/support/generator.js
@@ -18,6 +18,7 @@ Generator.prototype.prompts = function() {
       name:         'GeneratorTest.com',
       domain:       'generatortest.com',
       ip:           '192.168.137.137',
+      genesis:      'git@github.com/genesis/wordpress.git#69-tests',
       DB_NAME:      'generator_test',
       DB_USER:      'generator_test',
       DB_PASSWORD:  'generator_test'
