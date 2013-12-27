64cc1db6543982e157d8ddddfcacc88f4cb84b7d
Eric Clemmons <eric@smarterspam.com>
2013-12-26 22:17:11 -0600

    Generated test site correctly uses local & mounted library

--- test/support/generator.js
+++ test/support/generator.js
@@ -1,15 +1,25 @@
-#!/usr/bin/env node
-
+var fs      = require('fs');
 var hooker  = require('hooker');
-var path    = require('path');
 var helpers = require('yeoman-generator').test;
+var path    = require('path');
 
-var Generator = function() {};
+var Generator = function(outputDir) {
+  this.outputDir = __dirname + '/../temp';
+};
 
 Generator.prototype.create = function() {
   this.app = helpers.createGenerator('genesis-wordpress:app', [
     [require('../../generator/app'), 'genesis-wordpress:app']
   ]);
+
+  this.app.options['skip-install'] = true;
+};
+
+Generator.prototype.prepare = function() {
+  this.privatePath  = this.outputDir + '/provisioning/files/ssh/id_rsa';
+  this.privateKey   = fs.existsSync(this.privatePath) ? fs.readFileSync(this.privatePath) : null;
+  this.publicPath   = this.outputDir + '/provisioning/files/ssh/id_rsa.pub';
+  this.publicKey    = fs.existsSync(this.publicPath) ? fs.readFileSync(this.publicPath) : null;
 };
 
 Generator.prototype.prompts = function() {
@@ -44,17 +54,55 @@ Generator.prototype.prompts = function() {
 };
 
 Generator.prototype.run = function() {
-  helpers.testDirectory(path.join(__dirname, '..', 'temp'), function(err) {
+  helpers.testDirectory(this.outputDir, function(err) {
     if (err) {
       throw err;
     }
 
     this.create();
+    this.prepare();
     this.prompts();
 
-    this.app.run({}, function() {});
+    this.app.run({}, this.finalize.bind(this));
   }.bind(this));
 };
 
+Generator.prototype.finalize = function() {
+  fs.appendFileSync(this.outputDir + '/deployment/deploy.rb', [
+    '',
+    '# Use local repository for testing',
+    'set :deploy_via,        :copy',
+    'set :repository,        "."',
+    'set :local_repository,  "."',
+    // '# set :check_revision,    false',
+    // '# set :branch,            "master"',
+    ''
+  ].join('\n'));
+
+  if (this.privateKey) {
+    fs.writeFileSync(this.privatePath, this.privateKey);
+  }
+
+  if (this.publicKey) {
+    fs.writeFileSync(this.publicPath, this.publicKey);
+  }
+
+  var vagrantFile = fs.readFileSync(this.outputDir + '/Vagrantfile', 'utf8')
+    .replace(
+      new RegExp('(# Remount)'),
+      [
+        '# Mount library for testing',
+        '    box.vm.synced_folder "../../", "/wordpress", :nfs => true',
+        '',
+        '    $1'
+      ].join('\n')
+    )
+  ;
+
+  fs.writeFileSync(this.outputDir + '/Vagrantfile', vagrantFile);
+
+  fs.mkdirSync(this.outputDir + '/bower_components');
+  fs.symlinkSync('../../../../wordpress', this.outputDir + '/bower_components/genesis-wordpress');
+};
 
 module.exports = Generator;
