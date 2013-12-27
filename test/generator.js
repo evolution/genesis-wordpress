0fd090f2f6bb3fac47714ec005064c95c208b6a2
Eric Clemmons <eric@smarterspam.com>
2013-12-26 23:08:20 -0600

    Clean up test namespacing

--- test/generator.js
+++ test/generator.js
@@ -4,27 +4,25 @@ var assert    = require('assert');
 var fs        = require('fs');
 var path      = require('path');
 
-describe('Genesis WordPress', function () {
-  describe('generator', function() {
-    it('should create required files', function(done) {
-      [
-        'bin/provision',
-        'deployment/deploy.rb',
-        'provisioning/provision.yml',
-        'provisioning/files/ssh/id_rsa',
-        'provisioning/files/ssh/id_rsa.pub',
-        'web/wp-config.php',
-        'bower.json',
-        'Capfile',
-        'README.md',
-        'Vagrantfile',
-      ].forEach(function(file) {
-        var filePath = path.join(__dirname, 'temp', file);
+describe('Generator', function() {
+  it('should create required files', function(done) {
+    [
+      'bin/provision',
+      'deployment/deploy.rb',
+      'provisioning/provision.yml',
+      'provisioning/files/ssh/id_rsa',
+      'provisioning/files/ssh/id_rsa.pub',
+      'web/wp-config.php',
+      'bower.json',
+      'Capfile',
+      'README.md',
+      'Vagrantfile',
+    ].forEach(function(file) {
+      var filePath = path.join(__dirname, 'temp', file);
 
-        assert.ok(fs.existsSync(filePath), 'File not created: ' + filePath);
-      });
-
-      done();
+      assert.ok(fs.existsSync(filePath), 'File not created: ' + filePath);
     });
+
+    done();
   });
 });
--- test/support/bootstrap.js
+++ test/support/bootstrap.js
@@ -5,43 +5,41 @@ var Browser   = require('zombie');
 var fs        = require('fs');
 var path      = require('path');
 
-describe('Genesis WordPress', function () {
-  describe('site', function() {
-    it('may not be installed', function(done) {
-      var browser = new Browser();
+describe('Mock site', function() {
+  it('may not be installed', function(done) {
+    var browser = new Browser();
 
-      this.timeout(0);
+    this.timeout(0);
 
-      browser
-        .visit('http://local.generatortest.com/wp-admin/install.php')
-        .then(function() {
-          if (browser.button('Install WordPress')) {
-            browser
-              .fill('Site Title',       'Genesis WordPress Test')
-              .fill('Username',         'test')
-              .fill('admin_password',   'test')
-              .fill('admin_password2',  'test')
-              .fill('Your E-mail',      'test@example.com')
-              .uncheck('blog_public')
-            ;
+    browser
+      .visit('http://local.generatortest.com/wp-admin/install.php')
+      .then(function() {
+        if (browser.button('Install WordPress')) {
+          browser
+            .fill('Site Title',       'Genesis WordPress Test')
+            .fill('Username',         'test')
+            .fill('admin_password',   'test')
+            .fill('admin_password2',  'test')
+            .fill('Your E-mail',      'test@example.com')
+            .uncheck('blog_public')
+          ;
 
-            return browser.pressButton('Install WordPress');
-          }
-        })
-        .then(done, done)
-      ;
-    });
-
-    it('should be installed', function(done) {
-      var browser = new Browser();
-
-      browser
-        .visit('http://local.generatortest.com/wp-admin/install.php')
-        .then(function() {
-          assert.equal('Log In', browser.text('a.button'));
-        })
-        .then(done, done)
-      ;
-    })
+          return browser.pressButton('Install WordPress');
+        }
+      })
+      .then(done, done)
+    ;
   });
+
+  it('should be installed', function(done) {
+    var browser = new Browser();
+
+    browser
+      .visit('http://local.generatortest.com/wp-admin/install.php')
+      .then(function() {
+        assert.equal('Log In', browser.text('a.button'));
+      })
+      .then(done, done)
+    ;
+  })
 });
