6635d308e7725944fe31eded2d6d6269b49a09c8
Eric Clemmons <eric@smarterspam.com>
2014-07-15 22:21:06 -0500

    More tests

--- test/functional/varnish/cache.js
+++ test/functional/varnish/cache.js
@@ -2,20 +2,38 @@
 
 var assert    = require('assert');
 var Browser   = require('zombie');
-var fs        = require('fs');
-var path      = require('path');
 
 describe('Varnish', function() {
   it('should cache public page', function(done) {
     var browser = new Browser();
 
-    this.timeout(0);
-
     browser
       .visit('http://local.generatortest.com/')
       .then(function() {
+        return browser.reload();
+      }).then(function() {
         assert.equal('Hello world!', browser.text('#content h1'));
         assert.equal('cached', browser.resources.shift().response.headers['x-cache']);
+      })
+      .then(done, done)
+    ;
+  });
+
+  it('should not cache with wordpress cookies', function(done) {
+    var browser = new Browser();
+
+    browser
+      .visit('http://local.generatortest.com/wp-admin')
+      .then(function() {
+        assert(browser.resources.browser.getCookie('wordpress_test_cookie'));
+      })
+      .then(function() {
+        return browser.visit('http://local.generatortest.com/');
+      })
+      .then(function() {
+        assert.equal('Hello world!', browser.text('#content h1'));
+        assert.equal(0, browser.resources.shift().response.headers.age);
+        assert.equal('uncached', browser.resources.shift().response.headers['x-cache']);
       })
       .then(done, done)
     ;
---
+++ test/functional/wordpress/relative.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var assert    = require('assert');
+var Browser   = require('zombie');
+
+describe('WordPress', function() {
+  it('should use relative URLs in links', function(done) {
+    var browser = new Browser();
+
+    browser
+      .visit('http://local.generatortest.com/')
+      .then(function() {
+        assert.equal('/hello-world/', browser.query('#content h1 a').getAttribute('href'));
+      })
+      .then(done, done)
+    ;
+  });
+});
