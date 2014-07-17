1d47e3c7a8400dd853cff8cb2f6e7eaee9c389c6
Eric Clemmons <eric@smarterspam.com>
2014-07-16 22:38:05 -0500

    Working cookie tests

--- test/functional/varnish/cache.js
+++ test/functional/varnish/cache.js
@@ -4,7 +4,7 @@ var assert    = require('assert');
 var Browser   = require('zombie');
 
 describe('Varnish', function() {
-  it('should see public page', function(done) {
+  it('should access backend', function(done) {
     var browser = new Browser();
 
     browser
@@ -16,35 +16,103 @@ describe('Varnish', function() {
     ;
   });
 
-  it('should cache public page', function(done) {
-    var browser = new Browser();
+  describe('with no cookies', function() {
+    it('should cache', function(done) {
+      var browser = new Browser();
 
-    browser
-      .visit('http://local.generatortest.com/')
-      .then(function() {
-        assert.equal('cached', browser.resources[0].response.headers['x-cache']);
-      })
-      .then(done, done)
-    ;
+      browser
+        .visit('http://local.generatortest.com/')
+        .then(function() {
+          assert.equal('cached', browser.resources[0].response.headers['x-cache']);
+        })
+        .then(done, done)
+      ;
+    });
   });
 
-  it('should not cache with wordpress cookies', function(done) {
-    var browser = new Browser();
+  describe('with WordPress cookies', function() {
+    it('should not cache', function(done) {
+      var browser = new Browser();
 
-    browser
-      .visit('http://local.generatortest.com/wp-admin')
-      .then(function() {
-        assert(browser.resources.browser.getCookie('wordpress_test_cookie'));
-      })
-      .then(function() {
-        return browser.visit('http://local.generatortest.com/');
-      })
-      .then(function() {
-        assert.equal('Hello world!', browser.text('#content h1'));
-        assert.equal(0, browser.resources.shift().response.headers.age);
-        assert.equal('uncached', browser.resources.shift().response.headers['x-cache']);
-      })
-      .then(done, done)
-    ;
+      browser
+        .visit('http://local.generatortest.com/wp-admin')
+        .then(function() {
+          assert(browser.resources.browser.getCookie('wordpress_test_cookie'));
+        })
+        .then(function() {
+          return browser.visit('http://local.generatortest.com/');
+        })
+        .then(function() {
+          assert.equal('Hello world!', browser.text('#content h1'));
+          assert.equal(0, browser.resources[0].response.headers.age);
+          assert.equal('uncached', browser.resources[0].response.headers['x-cache']);
+        })
+        .then(done, done)
+      ;
+    });
+  });
+
+  describe('with tracking cookies', function() {
+    it('should ignore tracking cookies for cache', function(done) {
+      var browser = new Browser();
+
+      browser.setCookie({
+        name: '_test',
+        value: +new Date(),
+        domain: 'local.generatortest.com',
+        path: '/',
+      });
+
+      browser
+        .visit('http://local.generatortest.com/')
+        .then(function() {
+          assert(browser.getCookie('_test'));
+          assert(browser.resources[0].response.headers.age);
+          assert.equal('cached', browser.resources[0].response.headers['x-cache']);
+        })
+        .then(done, done)
+      ;
+    });
+  });
+
+  describe('with an application cookies', function() {
+    var cookie  = {
+      name: 'test',
+      value: +new Date(),
+      domain: 'local.generatortest.com',
+      path: '/',
+    };
+
+    it('should not be cached initially', function(done) {
+      var browser = new Browser();
+
+      browser.setCookie(cookie);
+
+      browser
+        .visit('http://local.generatortest.com/')
+        .then(function() {
+          assert(browser.getCookie('test'));
+          assert.equal(0, browser.resources[0].response.headers.age);
+          assert.equal('uncached', browser.resources[0].response.headers['x-cache']);
+        })
+        .then(done, done)
+      ;
+    });
+
+    it('should be subsequently cached', function(done) {
+      var browser = new Browser();
+
+      browser.setCookie(cookie);
+
+      browser
+        .visit('http://local.generatortest.com/')
+        .then(function() {
+          assert(browser.getCookie('test'));
+          assert(browser.resources[0].response.headers.age);
+          assert.equal('cached', browser.resources[0].response.headers['x-cache']);
+        })
+        .then(done, done)
+      ;
+    });
   });
 });
