884f3127ef54d4d068086b0f096d8c8f3b36b367
Eric Clemmons <eric@smarterspam.com>
2014-07-22 10:37:41 -0500

    Remove .only

--- test/functional/04-genesis.up.js
+++ test/functional/04-genesis.up.js
@@ -3,7 +3,7 @@
 var assert  = require('assert');
 var exec    = require('child_process').exec;
 
-describe.only('cap production genesis:up', function(done) {
+describe('cap production genesis:up', function(done) {
   it('should not fail', function(done) {
     exec('bundle exec cap production genesis:up', {
       cwd: process.cwd() + '/temp'
