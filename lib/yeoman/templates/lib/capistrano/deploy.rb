59af65d786dcddcca63536713c26c1c76f1470df
Evan Kaufman <evan@digitalflophouse.com>
2014-07-23 09:19:32 -0500

    deploy_to path namespaced within stage

--- lib/yeoman/templates/lib/capistrano/deploy.rb
+++ lib/yeoman/templates/lib/capistrano/deploy.rb
@@ -4,4 +4,4 @@ lock '3.2.1'
 # Repository name
 set :application,   "<%= props.name %>"
 set :domain,        "<%= props.domain %>"
-set :deploy_to,     "/var/www/#{fetch(:domain)}/#{fetch(:branch)}"
+set :deploy_to,     "/var/www/#{fetch(:domain)}/#{fetch(:stage)}/#{fetch(:branch)}"
