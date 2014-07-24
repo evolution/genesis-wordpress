4fd8a42a728cd97193dd21bf5427c988cabd79db
Evan Kaufman <evan@digitalflophouse.com>
2014-07-24 09:14:11 -0500

    Added bower install after deploy:updated

--- lib/yeoman/templates/lib/capistrano/deploy.rb
+++ lib/yeoman/templates/lib/capistrano/deploy.rb
@@ -6,3 +6,11 @@ set :application,   "<%= props.name %>"
 set :domain,        "<%= props.domain %>"
 set :deploy_to,     "/var/www/#{fetch(:domain)}/#{fetch(:stage)}/#{fetch(:branch)}"
 set :wp_path,       "#{release_path}/web/wp"
+
+namespace :deploy do
+  after :updated, :bower_install do
+    on roles(:web) do
+      execute "cd #{release_path} && bower install --config.interactive=false"
+    end
+  end
+end
