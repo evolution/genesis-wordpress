4842c323494b914b3714064aadffe4d8186523e1
Evan Kaufman <evan@digitalflophouse.com>
2014-07-24 11:47:51 -0500

    changed order of tests, added longer timeout for deploy

--- test/functional/04-deploy.js
+++ test/functional/04-deploy.js
@@ -5,6 +5,8 @@ var exec    = require('child_process').exec;
 
 describe('cap production deploy', function(done) {
   it('should not fail', function(done) {
+    this.timeout(60 * 1000);
+
     exec('bundle exec cap production deploy', {
       cwd: process.cwd() + '/temp'
     }, function(err, stdout, stderr) {
